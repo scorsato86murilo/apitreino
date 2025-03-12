dcda0862abcfc89e81a4e2a068fa71730d81ecbe
Murilo Scorsato <scorsato86murilo@gmail.com>
2025-03-12 20:03:34 -0300

    algumasmodels criada para o index

--- index/models.py
+++ index/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Index(models.Model):
+    cidade_estado = models.CharField(max_length=100)
+    rua_numero = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    img = models.ImageField(upload_to='logo')
+
+    def __str__(self):
+        return self.cidade_estado
+
+class Avisos(models.Model):
+    mensagem = models.TextField()
+
+    def __str__(self):
+        return self.mensagem
+
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    telefone = models.CharField(max_length=100)
+    rua = models.CharField(max_length=100)
+    numero = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
